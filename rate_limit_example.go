cee43405c146e4822136a4e8a69a9a652b0ee220
aditya <dev@chimeracoder.net>
2013-12-01 13:02:58 -0500

    Update rate_limit_example.go to use time.After instead of time.Sleep

--- rate_limit_example.go
+++ rate_limit_example.go
@@ -1,17 +1,18 @@
 //Execute a query that will automatically be throttled
-func throttledQuery(queryQueue chan queryChan) {
+func (c *TwitterApi) throttledQuery(queryQueue chan queryChan) {
     for q := range queryQueue {
 
         endpoint_path := q.endpoint_path
         method := q.method
         keyVals := q.keyVals
         response_ch := q.response_ch
-        result, err := Query(endpoint_path, method, keyVals)
+        err := c.execQuery(url, form, data, method)
         response_ch <- struct {
             result []byte
-                err    error
+            err    error
         }{result, err}
 
-        time.Sleep(SECONDS_PER_QUERY)
+        // Reading from this will block until the specified time has elapsed
+        <-time.After(SECONDS_PER_QUERY)
     }
 }
