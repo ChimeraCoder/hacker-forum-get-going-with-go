b92aa440a296bd223590b4bc0b96ff877a4089d4
aditya <dev@chimeracoder.net>
2013-12-02 12:37:51 -0500

    Update interface example to show function call

--- interface_example.go
+++ interface_example.go
@@ -11,3 +11,13 @@ func (destination CheckingAccount) Deposit(amount int) error {
     destination.Balance += amount
     return nil
 }
+
+// We want to donate money to any account type (Checking, Savings)
+// Donations require a tax receipt
+func Donate(recipient Account, amount int) error {
+    err := recipient.Deposit(amount)
+    if err != nil{
+        return err
+    }
+    IssueTaxReceipt(amount)
+}
